d2b0b730d5c804f4d9eaf324fb6ce4b496d8b928
Christophe-Desmarres <chrisdmar12@gmail.com>
2022-03-16 23:32:57 +0100

    arrangement media 900px

--- js/theme.js
+++ js/theme.js
@@ -1,14 +1,16 @@
+let mode = 'jour';
 
 
-       let mode = "jour";
 
-       function colorMode() {
+     function colorMode() {
+           
         let inter = document.getElementsByClassName('inter');
         let portrait = document.getElementsByClassName('portrait');
         let container = document.getElementsByClassName('container');
         let retour = document.getElementsByClassName('retour');
         let h2 = document.querySelectorAll('h2');
         let lien = document.querySelectorAll('a');
+        let timeTag = document.querySelectorAll('time');
 
         if (mode == "jour") {
                mode = "nuit";
@@ -16,10 +18,12 @@
                new Audio('https://assets.codepen.io/165585/switch.mp3').play();
                document.body.style.backgroundColor = 'rgba(9, 9, 17, 0.959)';
                document.body.style.color = "white";
+               //chgt d'icone avec la lune
                document.getElementsByClassName("inter")[0].src = '../image/icones/icone-moon.png';
+               //chgt style background-color et box-shadow
                inter[0].style.backgroundColor = "rgba(9, 9, 17, 0.959)";
                inter[0].style.boxShadow = "inset 0px 4px 10px white";
-               container[0].style.boxShadow = "4px 4px 7px rgb(255, 217, 0)";
+               container[0].style.boxShadow = "4px 4px 10px rgb(255, 217, 0)";
                h2.forEach(element => {
                    element.style.color = "black";
                    element.style.backgroundColor = "aquamarine";
@@ -36,10 +40,13 @@
             
             if (document.title == 'CV Christophe Desmarres') {
                portrait[0].style.boxShadow = "2px 2px 5px rgb(255, 217, 0)";
+               timeTag.forEach(element => {
+                   element.style.borderBottom =  "1px solid white";
+               });
 
             } else if (document.title == 'galerie') {
                 retour[0].style.backgroundColor = "white";
-
+               
             }
 
             
@@ -72,6 +79,9 @@
                 
                 if (document.title == 'CV Christophe Desmarres') {
                 portrait[0].style.boxShadow = "2px 2px 5px black";
+                timeTag.forEach(element => {
+                    element.style.borderBottom =  "1px solid black";
+                });
 
                 } else if (document.title == 'galerie') {
                 retour[0].style.color = "black";
@@ -79,6 +89,7 @@
             }
 
             }
+            return mode;
         }
         
 
