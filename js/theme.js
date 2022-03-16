8adbaad6fefbf8b912152d394154f9afaa098cc8
Christophe-Desmarres <chrisdmar12@gmail.com>
2022-03-16 18:16:07 +0100

    debut mise en page >900px

--- js/theme.js
+++ js/theme.js
@@ -2,7 +2,7 @@
 
        let mode = "jour";
 
-       function colorMode(title) {
+       function colorMode() {
         let inter = document.getElementsByClassName('inter');
         let portrait = document.getElementsByClassName('portrait');
         let container = document.getElementsByClassName('container');
@@ -12,13 +12,13 @@
 
         if (mode == "jour") {
                mode = "nuit";
-               console.log("mode nuit");
-               console.log("underline");
+               console.log("thème sombre activé");
                new Audio('https://assets.codepen.io/165585/switch.mp3').play();
                document.body.style.backgroundColor = 'rgba(9, 9, 17, 0.959)';
                document.body.style.color = "white";
                document.getElementsByClassName("inter")[0].src = '../image/icones/icone-moon.png';
                inter[0].style.backgroundColor = "rgba(9, 9, 17, 0.959)";
+               inter[0].style.boxShadow = "inset 0px 4px 10px white";
                container[0].style.boxShadow = "4px 4px 7px rgb(255, 217, 0)";
                h2.forEach(element => {
                    element.style.color = "black";
@@ -46,12 +46,13 @@
         
         } else {
                 mode = "jour";
-                console.log("mode jours");
+                console.log("thème clair activé");
                 new Audio('https://assets.codepen.io/165585/switch.mp3').play();
                 document.body.style.backgroundColor = "white";
                 document.body.style.color = "black";
                 document.getElementsByClassName("inter")[0].src = '../image/icones/icone-soleil.png';
                 inter[0].style.backgroundColor = "white";
+                inter[0].style.boxShadow = "inset 0px 4px 10px black";
                 container[0].style.boxShadow = "4px 4px 7px black";
                 
                 h2.forEach(element => {
@@ -80,56 +81,6 @@
             }
         }
         
-        // mode nuit
-
-        // document.querySelectorAll("a").style.color = "white";
-        // document.links.style.color = "white";
-        // document.querySelectorAll(".underline::before").style.backgroundColor= "white";
-        //    document.getElementsByClassName("portrait").style.boxShadow = "white";
-        // document.getElementsByClassName("lien_reseau a").style.borderBottom = "white";
-        //    document.getElementsByTagName("h2").style.color = "black";
-        //    document.getElementsByTagName("h2").style.backgroundColor = "aquamarine";
-        //    document.getElementsByTagName("h2").style.borderBottom = "black";
-        //    document.getElementsByTagName("h2").style.borderLeft = "black";
-        //    document.getElementsByTagName("h2").style.boxShadow = "aquamarine";
-        //    document.getElementsByClassName("time").style.borderBottom = "white";
-        //    document.querySelectorAll('#text').style.color = "white";
-
-        // couleursdoc.style.txtColor = "white";
-        // couleursdoc.style.h2Color = "aquamarine";
-        // couleursdoc.style.h2BgColor = "black";
-        // couleursdoc.style.n2BorderColor = "aquamarine";
-        // couleursdoc.style.shadow = "black";
-        // couleursdoc.style.borderUnderline = "black";
-        
-
-        // mode jour
-                       // document.querySelectorAll("a").style.color = "black";
-                       // document.links.style.color = "black";
-                       // document.querySelectorAll(".underline::before").style.backgroundColor = "black";
-                    //    document.getElementsByClassName("portrait").style.boxShadow = "black";
-                       // document.getElementsByClassName("lien_reseau a").style.borderBottom = "black";
-                    //    document.getElementsByTagName("h2").style.color = "aquamarine";
-                    //    document.getElementsByTagName("h2").style.backgroundColor = "black";
-                    //    document.getElementsByTagName("h2").style.borderBottom = "aquamarine";
-                    //    document.getElementsByTagName("h2").style.borderLeft = "aquamarine";
-                    //    document.getElementsByTagName("h2").style.boxShadow = "black";
-                    //    document.getElementsByClassName("time").style.borderBottom = "black";
-                    //    document.querySelectorAll('#text').style.color = "black";
-        
-                       // couleursdoc.style.txtColor = "black";
-                       // couleursdoc.style.h2Color = "aquamarine";
-                       // couleursdoc.style.h2BgColor = "black";
-                       // couleursdoc.style.n2BorderColor = "aquamarine";
-                       // couleursdoc.style.shadow = "black";
-                       // couleursdoc.style.borderUnderline = "black";
-
-
-
-
-
-
-
 
 //-----------------------------------------------
 
